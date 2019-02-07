f85697f51b4e2d44ce80dfed0910748a5f00300c
Gabriel Panji Lazuardi <Bephometh@users.noreply.github.com>
2019-02-07 13:34:33 +0700

    Pengerjaan dummy test case

--- www/application/controllers/UnitTest.php
+++ www/application/controllers/UnitTest.php
@@ -7,32 +7,28 @@ class UnitTest extends CI_Controller{
 	public function __construct(){
 		parent::__construct();
 		$this->load->library("unit_test");
-
+		$this->load->database();
+		//$this->load->model('Transkrip_model');
+		$this->load->library('BlueTape');
+		
 	}
 
-	public function start_test(){
-		$this->load->database();
-		$this->load->model('Transkrip_model');
-		$this->load->libraries('BlueTape')
-		$test = $this->BlueTape->getNPM()
-		//$this->Transkrip_model->requestsBy('7316068@student.unpar.ac.id',NULL,NULL);
-		
-		print_r($test);
-     /**$this->Transkrip_model->requestBy('7316068@student.unpar.ac.id',NULL,NULL);**/
+	public function index(){
+		$this->checkNPMandEMAIL();
+	}
 
+	public function checkNPMandEMAIL(){
+		$userInfo = $this->Auth_model->getUserInfo();
+	   $test = $this->bluetape->getNPM($userInfo['email'],NULL);
+	
 		$arr= array('7316068@student.unpar.ac.id','2016730086','GABRIEL PANJI LAZUARDI');
-		$expected_result = $arr;//array( [0] => stdClass Object ( [id] => 1 [requestByEmail] => '7316068@student.unpar.ac.id' 
-				//[requestDateTime] => 2019-01-29 14:22:17 [requestType] => 'DPS_EN' [requestUsage] => 'Untuk Unit Testing'
-				//[answer] => 'printed' [answeredByEmail] => 'gabriel.lazuardi@tu.ftis.unpar' [answeredDateTime] => 2019-01-08 00:00:00 [answeredMessage] => 'Accepted unit test' ) );
-
-		$test_name = 'Test to see if the request maker is inside the database';
+		$expected_result = '2016730068';
+		$test_name = 'Mengecek apakah NPM sama Email Sama pemohon sesuai';
 
 		$this->unit->run($test, $expected_result, $test_name);
 
 		echo $this->unit->report();
 	}   
-
-
 }
 	
 
