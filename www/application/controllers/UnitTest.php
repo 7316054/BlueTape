beef5b87b0ff634263a944285c49f131417e0caa
Gabriel Panji Lazuardi <Bephometh@users.noreply.github.com>
2019-02-07 12:36:55 +0700

    pemindahan file

--- www/application/controllers/UnitTest.php
+++ www/application/controllers/UnitTest.php
@@ -11,19 +11,28 @@ class UnitTest extends CI_Controller{
 	}
 
 	public function start_test(){
+		$this->load->database();
+		$this->load->model('Transkrip_model');
+		$this->load->libraries('BlueTape')
+		$test = $this->BlueTape->getNPM()
+		//$this->Transkrip_model->requestsBy('7316068@student.unpar.ac.id',NULL,NULL);
 		
-		$test = 1 + 1;
+		print_r($test);
+     /**$this->Transkrip_model->requestBy('7316068@student.unpar.ac.id',NULL,NULL);**/
 
-		$expected_result = 2;
+		$arr= array('7316068@student.unpar.ac.id','2016730086','GABRIEL PANJI LAZUARDI');
+		$expected_result = $arr;//array( [0] => stdClass Object ( [id] => 1 [requestByEmail] => '7316068@student.unpar.ac.id' 
+				//[requestDateTime] => 2019-01-29 14:22:17 [requestType] => 'DPS_EN' [requestUsage] => 'Untuk Unit Testing'
+				//[answer] => 'printed' [answeredByEmail] => 'gabriel.lazuardi@tu.ftis.unpar' [answeredDateTime] => 2019-01-08 00:00:00 [answeredMessage] => 'Accepted unit test' ) );
 
-		$test_name = 'Adds one plus one';
+		$test_name = 'Test to see if the request maker is inside the database';
 
 		$this->unit->run($test, $expected_result, $test_name);
 
 		echo $this->unit->report();
-	}
+	}   
 
-	
+
 }
 	
 
