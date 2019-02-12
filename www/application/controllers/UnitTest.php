1a68437547e1159398712d4021a4fcf893c3d662
Gabriel Panji Lazuardi <Bephometh@users.noreply.github.com>
2019-02-12 15:58:03 +0700

    Pembuatan test untuk method requestBy dari jadwal_dosenModel

--- www/application/controllers/UnitTest.php
+++ www/application/controllers/UnitTest.php
@@ -8,27 +8,63 @@ class UnitTest extends CI_Controller{
 		parent::__construct();
 		$this->load->library("unit_test");
 		$this->load->database();
-		//$this->load->model('Transkrip_model');
+		$this->load->model('JadwalDosen_model');
 		$this->load->library('BlueTape');
 		
 	}
 
 	public function index(){
-		$this->checkNPMandEMAIL();
+		$this->requestByDosen('Samuel');
+		print_r($this->unit->result());
 	}
 
-	public function checkNPMandEMAIL(){
-		$userInfo = $this->Auth_model->getUserInfo();
-	   $test = $this->bluetape->getNPM($userInfo['email'],NULL);
-	
-		$arr= array('7316068@student.unpar.ac.id','2016730086','GABRIEL PANJI LAZUARDI');
-		$expected_result = '2016730068';
-		$test_name = 'Mengecek apakah NPM sama Email Sama pemohon sesuai';
+	/**
+	 * Method untuk memeriksa method requestBy pada model jadwal_dosen
+	 * @var adalaha nama dari dosen
+	 * Expected result merupakan Array dari hasil query
+	**/
+	public function requestByDosen($var){
+		
+	    $test = $this->JadwalDosen_model->requestsBy($var);
+		//print_r($test);
+		$expected_result = $this->expectedResDosen($var);
+		$test_name = 'Memeriksa method requestBy dari JadwalDosen_model';
 
 		$this->unit->run($test, $expected_result, $test_name);
 
-		echo $this->unit->report();
+		
 	}   
+
+	public function runTest(){
+		$userInfo = $this->Auth_model->getUserInfo();
+		$arr = $this->Transkrip_model->requestsBy('7316068@student.unpar.ac.id');
+	    for($i=0; $i<sizeof($arr);$i++){ 
+					$row=$arr[$i]; 
+					print_r($row->requestByEmail." "); 
+					$this->checkNPMandEMAIL($row->requestByEmail);
+		};
+
+	}
+
+	/**
+	 * Method untuk mendapatkan expected result dari test requestByDosen
+	 * @var adalaha nama dari dosen
+	 * Expected result merupakan Array dari hasil query
+	**/
+	public function expectedResDosen($var){
+		// cara 1
+		$query1 = $this->db->query("SELECT * FROM jadwal_dosen WHERE user = '$var' ORDER BY durasi DESC");
+		//cara 2
+		$this->db->where('user', $var);
+		$this->db->from('jadwal_dosen');
+		$this->db->order_by('durasi', 'DESC');
+		$query2 = $this->db->get();
+
+		//ubah $query menjadi  $query1 atau  $query2 untuk memilih cara query yang digunakan
+		$res = $query1->result();
+		//print_r($res);
+		return $res;
+	}
 }
 	
 
