59b0737fb007b4bb38d128b34810b73226bb1dfc
Gabriel Panji Lazuardi <Bephometh@users.noreply.github.com>
2019-02-14 14:05:29 +0700

    Penabahan email kedalam config/module dana penamabahan pengujian test case

--- www/application/controllers/UnitTest.php
+++ www/application/controllers/UnitTest.php
@@ -21,6 +21,9 @@
          $this->cekJadwalByUsername('Dipo');
          $this->cekYearMonthToSemesterCode();
          $this->cekSemesterCodeToString();
+		 $this->requestByDosen('Samuel');
+		$this->getName('GABRIEL PANJI LAZUARDI');
+		$this->dbDateTimeToReadableDate();
          print_r($this->unit->result());
        }
        /**
@@ -98,7 +101,102 @@
             __FUNCTION__,
             'angkatan 2016'
         );
-       }
+		/**
+	 * Method untuk memeriksa method requestBy pada model jadwal_dosen
+	 * @var adalaha nama dari dosen
+	 * Expected result merupakan Array dari hasil query
+	**/
+	public function requestByDosen($var){
+		
+	    $test = $this->JadwalDosen_model->requestsBy($var);
+		//print_r($test);
+		$expected_result = $this->expectedResDosen($var);
+		$test_name = 'Memeriksa method requestBy dari JadwalDosen_model';
+
+		$this->unit->run($test, $expected_result, $test_name);
+	}   
+
+	/**
+	 * Method untuk memeriksa method getName pada libraries/BlueTape
+	 * @var adalaha nama dari user BlueTape
+	 * Expected result merupakan  hasil query
+	**/
+	public function getName($var){
+		$test = $this->bluetape->getName($var);
+		
+		$expected_result = $this->expectedResGetName($var);
+		$test_name = 'Memeriksa method getName dari BlueTape';
+
+		$this->unit->run($test, $expected_result, $test_name);
+	}
+
+	/**
+	 * Method untuk memeriksa method dbDateTimeToReadableDate pada libraries/BlueTape
+	 * Expected result merupakan  hasil konversi DateTime dari database ke dalam string yang dapat dibaca
+	**/
+	public function dbDateTimeToReadableDate(){
+		
+		$this->db->select('requestDateTime');
+		$this->db->from('transkrip');
+		$query = $this->db->get();
+		$dateTime = $query->row;
+
+		setlocale(LC_TIME, 'ind');
+		$expected_result = strftime('%A, %B, %Y',(new DateTime($dateTime->requestDateTime))->getTimestamp());
+		$test = $this->bluetape->dbDateTimeToReadableDate($dateTime->requestDateTime);
+		$test_name = 'Memeriksa method dbDateTimeToReadableDate dari BlueTape';
+
+		$this->unit->run($test, $expected_result, $test_name);
+
+	}
+
+
+
+
+
+
+
+
+
+	//--------------EXPECTED RESULTS-----------------------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Method untuk mendapatkan expected result dari test requestByDosen
+	 * @var adalaha nama dari dosen
+	 * Expected result merupakan Array dari hasil query
+	**/
+	public function expectedResDosen($var){
+		// cara 1
+		$query1 = $this->db->query("SELECT * FROM jadwal_dosen WHERE user = '$var' ORDER BY durasi DESC");
+		//cara 2
+		$this->db->where('user', $var);
+		$this->db->from('jadwal_dosen');
+		$this->db->order_by('durasi', 'DESC');
+		$query2 = $this->db->get();
+
+		//ubah $query menjadi  $query1 atau  $query2 untuk memilih cara query yang digunakan
+		$res = $query1->result();
+		//print_r($res);
+		return $res;
+	}
+
+	/**
+	 * Method untuk mendapatkan expected result dari test getName
+	 * @var adalaha nama dari user BlueTape
+	 * Expected result merupakan hasil query
+	**/
+	public function expectedResGetName($var){
+		$this->db->where('name',$var);
+		$this->db->from('bluetape_userinfo');
+
+		$query = $this->db->get();
+
+		$row = $query->row;
+
+		return $row->name;
+	}
+	
+	}
 
        
 
