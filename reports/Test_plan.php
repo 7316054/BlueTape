6dcc7159303322197f29f875892544a0dfced980
Gabriel Panji Lazuardi <Bephometh@users.noreply.github.com>
2019-03-26 15:12:41 +0700

    update gitignore

---
+++ reports/Test_plan.php
@@ -0,0 +1,181 @@
+
+<table style="width:100%; font-size:small; margin:10px 0; border-collapse:collapse; border:1px solid #CCC;">
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Name</th>
+		<td style="border-bottom:1px solid #CCC;">cekAddjadwal</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">Integer</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Expected Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">Integer</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Result</th>
+		<td style="border-bottom:1px solid #CCC;"><span style="color: #0C0;">Passed</span></td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">File Name</th>
+		<td style="border-bottom:1px solid #CCC;">C:\xampp\htdocs\BlueTape\www\application\controllers\UnitTest.php</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Line Number</th>
+		<td style="border-bottom:1px solid #CCC;">91</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Notes</th>
+		<td style="border-bottom:1px solid #CCC;">Test ini mengecek apakah data masuk atau tidak</td>
+	</tr>
+</table>
+<table style="width:100%; font-size:small; margin:10px 0; border-collapse:collapse; border:1px solid #CCC;">
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Name</th>
+		<td style="border-bottom:1px solid #CCC;">cekGetNpm</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">String</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Expected Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">String</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Result</th>
+		<td style="border-bottom:1px solid #CCC;"><span style="color: #0C0;">Passed</span></td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">File Name</th>
+		<td style="border-bottom:1px solid #CCC;">C:\xampp\htdocs\BlueTape\www\application\controllers\UnitTest.php</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Line Number</th>
+		<td style="border-bottom:1px solid #CCC;">99</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Notes</th>
+		<td style="border-bottom:1px solid #CCC;">Test ini mengecek apakah NPM valid atau tidak</td>
+	</tr>
+</table>
+<table style="width:100%; font-size:small; margin:10px 0; border-collapse:collapse; border:1px solid #CCC;">
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Name</th>
+		<td style="border-bottom:1px solid #CCC;">cekGetNpm</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">String</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Expected Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">String</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Result</th>
+		<td style="border-bottom:1px solid #CCC;"><span style="color: #0C0;">Passed</span></td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">File Name</th>
+		<td style="border-bottom:1px solid #CCC;">C:\xampp\htdocs\BlueTape\www\application\controllers\UnitTest.php</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Line Number</th>
+		<td style="border-bottom:1px solid #CCC;">104</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Notes</th>
+		<td style="border-bottom:1px solid #CCC;">Test ini mengecek apakah NPM valid atau tidak</td>
+	</tr>
+</table>
+<table style="width:100%; font-size:small; margin:10px 0; border-collapse:collapse; border:1px solid #CCC;">
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Name</th>
+		<td style="border-bottom:1px solid #CCC;">cekGetNpm</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">String</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Expected Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">String</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Result</th>
+		<td style="border-bottom:1px solid #CCC;"><span style="color: #0C0;">Passed</span></td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">File Name</th>
+		<td style="border-bottom:1px solid #CCC;">C:\xampp\htdocs\BlueTape\www\application\controllers\UnitTest.php</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Line Number</th>
+		<td style="border-bottom:1px solid #CCC;">110</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Notes</th>
+		<td style="border-bottom:1px solid #CCC;">Test ini mengecek apakah NPM valid atau tidak</td>
+	</tr>
+</table>
+<table style="width:100%; font-size:small; margin:10px 0; border-collapse:collapse; border:1px solid #CCC;">
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Name</th>
+		<td style="border-bottom:1px solid #CCC;">cekYearMonthToSemesterCodeSimplified</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">String</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Expected Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">String</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Result</th>
+		<td style="border-bottom:1px solid #CCC;"><span style="color: #0C0;">Passed</span></td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">File Name</th>
+		<td style="border-bottom:1px solid #CCC;">C:\xampp\htdocs\BlueTape\www\application\controllers\UnitTest.php</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Line Number</th>
+		<td style="border-bottom:1px solid #CCC;">121</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Notes</th>
+		<td style="border-bottom:1px solid #CCC;">Test ini mengecek Konversi tahun dan bulan ke kode semester, disederhanakan menjadi dua semester</td>
+	</tr>
+</table>
+<table style="width:100%; font-size:small; margin:10px 0; border-collapse:collapse; border:1px solid #CCC;">
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Name</th>
+		<td style="border-bottom:1px solid #CCC;">cekYearMonthToSemesterCodeSimplified</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Test Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">String</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Expected Datatype</th>
+		<td style="border-bottom:1px solid #CCC;">String</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Result</th>
+		<td style="border-bottom:1px solid #CCC;"><span style="color: #0C0;">Passed</span></td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">File Name</th>
+		<td style="border-bottom:1px solid #CCC;">C:\xampp\htdocs\BlueTape\www\application\controllers\UnitTest.php</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Line Number</th>
+		<td style="border-bottom:1px solid #CCC;">129</td>
+	</tr>
+	<tr>
+		<th style="text-align: left; border-bottom:1px solid #CCC;">Notes</th>
+		<td style="border-bottom:1px solid #CCC;">Test ini mengecek Konversi tahun dan bulan ke kode semester, disederhanakan menjadi dua semester</td>
+	</tr>
+</table>
