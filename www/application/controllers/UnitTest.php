e41b0cb4e34935ff8809550731862588d0fd4130
samuel sihombing <7316054@student.unpar.ac.id>
2019-04-11 14:17:29 +0700

    membenarkan method checkrequestTypesForbidden

--- www/application/controllers/UnitTest.php
+++ www/application/controllers/UnitTest.php
@@ -283,21 +283,31 @@
 		public function checkRequestTypesForbidden(){
 			
 			//Test case 1
-			
-			$request = $this->Transkrip_model->requestsBy('7316053@student.unpar.ac.id');
-			$test1 = $this->Transkrip_model->requestTypesForbidden($request);
-			$expected_result1 = array( 'LHS', 'DPS_ID');
-			$test_name1 = 'Test ini berfungsi untuk memeriksa method requestTypeForbidden dari Transkrip_model yang keluarannya request type dari permintaan transkrip';
-			 $this->unit->run($test1, $expected_result1, $test_name1);
+   
+            $request = $this->Transkrip_model->requestsBy('7316091@student.unpar.ac.id');
+            $test1 = $this->Transkrip_model->requestTypesForbidden($request);
+            $expected_result1 = array( 'LHS');
+            $test_name1 = 'Test ini berfungsi untuk memeriksa method requestTypeForbidden dari Transkrip_model yang keluarannya request type dari permintaan transkrip';
+            $this->unit->run($test1, $expected_result1, $test_name1);
+                      
+            //Test Case2
+            $request = $this->Transkrip_model->requestsBy('7316054@student.unpar.ac.id');
+            $test2 = $this->Transkrip_model->requestTypesForbidden($request);
+            $expected_result2 ='Anda tidak bisa meminta cetak karena ada permintaan lain yang belum selesai.';
+            $test_name2 = 'Test ini berfungsi untuk memeriksa method requestTypeForbidden dari Transkrip_model dimana belum ada transkrip yang di jawab(answer)';
+            $this->unit->run($test2, $expected_result2, $test_name2);
+                   
+            //Test case 3
+            $date = getdate();
+            $currentYear = $date['year'];
+            $currentMonth = $date['mon'];
+            $currentSemester = $this->bluetape->yearMonthToSemesterCodeSimplified($currentYear, $currentMonth);      
+            $request = $this->Transkrip_model->requestsBy('7316053@student.unpar.ac.id');
+            $test3 = $this->Transkrip_model->requestTypesForbidden($request);
+            $expected_result3 ='Anda tidak bisa meminta cetak karena seluruh jenis transkrip sudah pernah dikabulkan di semester ini (' . $this->bluetape->semesterCodeToString($currentSemester) . ').';
+            $test_name3 = 'Test ini berfungsi untuk memeriksa method requestTypeForbidden dari Transkrip_model dimana seluruh jenis transkrip sudah terkabul';
+            $this->unit->run($test3, $expected_result3, $test_name3);
 
-			 //Test Case2
-			 $request = $this->Transkrip_model->requestsBy('7316054@student.unpar.ac.id');
-			$test2 = $this->Transkrip_model->requestTypesForbidden($request);
-			$expected_result2 =array();//'Anda tidak bisa meminta cetak karena ada permintaan lain yang belum selesai.';
-			$test_name2 = 'Test ini berfungsi untuk memeriksa method requestTypeForbidden dari Transkrip_model dimana belum ada transkrip yang di jawab(answer)';
-			 $this->unit->run($test2, $expected_result2, $test_name2);
-
-			 //Test case 3
 		}
 
 
