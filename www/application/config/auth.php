3d882538d4414c9bf6e43a037a321123bdee6565
Gabriel Panji Lazuardi <Bephometh@users.noreply.github.com>
2019-04-25 13:16:19 +0700

    degbu send email

--- www/application/config/auth.php
+++ www/application/config/auth.php
@@ -9,7 +9,7 @@ $config['google-redirecturi'] = $config['domain'] . '/auth/oauth2callback';
 
 $config['email-config'] = Array(
     'protocol' => 'smtp',
-    'smtp_host' => 'ssl://smtp.googlemail.com',
+    'smtp_host' => 'ssl://smtp.gmail.com',
     'smtp_port' => 465,
     'smtp_user' => 'rootbluetape@gmail.com',
     'smtp_pass' => 'Rootbluetape123',
