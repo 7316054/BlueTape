7d5b72cefd11ac753b7d2d8698e9d420660c33db
Anugrah Jaya <anugrahjaya23@gmail.com>
2019-04-25 13:34:54 +0700

    merge dengan master

--- www/application/config/auth-test.php
+++ www/application/config/auth-test.php
@@ -2,18 +2,18 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['domain'] = 'https://travis-ci.com';
+$config['domain'] = 'localhost';
 $config['google-clientid'] = '466653343196-620lga9usl4lsmr99o9k1kljrd94ecfj.apps.googleusercontent.com';
 $config['google-clientsecret'] = 'a8yaddxuxBLO9JR8to9vkxyT';
 $config['google-redirecturi'] = $config['domain'] . '/auth/oauth2callback';
 
 $config['email-config'] = Array(
     'protocol' => 'smtp',
-    'smtp_host' => 'smtp.googlemail.com',
-    'smtp_port' => 587,
+    'smtp_host' => 'ssl://smtp.googlemail.com',
+    'smtp_port' => 465,
     'smtp_user' => 'rootbluetape@gmail.com', //masukan email google 
     'smtp_pass' => 'Rootbluetape123', //password email  // di config bikin 2 AUTH 1 YANG SALAH
     'mailtype' => 'html',
     'charset' => 'iso-8859-1',
-    'smtp_crypto'=> 'tls'
+    // 'smtp_crypto'=> 'tls'
 );
