4d956e203716263b0fb3ba187a2a42975dd47c50
Gabriel Panji Lazuardi <Bephometh@users.noreply.github.com>
2019-04-25 13:02:24 +0700

    Test barbar

---
+++ www/application/config/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['domain'] = 'http://localhost';
+$config['google-clientid'] = '466653343196-620lga9usl4lsmr99o9k1kljrd94ecfj.apps.googleusercontent.com';
+$config['google-clientsecret'] = 'a8yaddxuxBLO9JR8to9vkxyT';
+$config['google-redirecturi'] = $config['domain'] . '/auth/oauth2callback';
+
+$config['email-config'] = Array(
+    'protocol' => 'smtp',
+    'smtp_host' => 'ssl://smtp.googlemail.com',
+    'smtp_port' => 587,
+    'smtp_user' => 'rootbluetape@gmail.com',
+    'smtp_pass' => 'Rootbluetape123',
+    'mailtype' => 'html',
+    'charset' => 'iso-8859-1'
+);
