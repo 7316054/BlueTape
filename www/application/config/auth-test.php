edec6a1b65d8f9dfc10a29356f6dccc01b52e73d
Anugrah Jaya <anugrahjaya23@gmail.com>
2019-04-30 15:49:49 +0700

    memperbaiki code untuk cek sendEmail

--- www/application/config/auth-test.php
+++ www/application/config/auth-test.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['domain'] = 'localhost';
+$config['domain'] = 'http://localhost';
 $config['google-clientid'] = '466653343196-620lga9usl4lsmr99o9k1kljrd94ecfj.apps.googleusercontent.com';
 $config['google-clientsecret'] = 'a8yaddxuxBLO9JR8to9vkxyT';
 $config['google-redirecturi'] = $config['domain'] . '/auth/oauth2callback';
@@ -10,7 +10,7 @@ $config['google-redirecturi'] = $config['domain'] . '/auth/oauth2callback';
 $config['email-config'] = Array(
     'protocol' => 'smtp',
     'smtp_host' => 'ssl://smtp.googlemail.com',
-    'smtp_port' => 587,
+    'smtp_port' => 465,
     'smtp_user' => 'rootbluetape@gmail.com', //masukan email google 
     'smtp_pass' => 'Rootbluetape123', //password email  // di config bikin 2 AUTH 1 YANG SALAH
     'mailtype' => 'html',
