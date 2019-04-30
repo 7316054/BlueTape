52b4516a5f507f203ca710ff7a580e1e32b1f47f
samuel sihombing <7316054@student.unpar.ac.id>
2019-04-30 15:12:05 +0700

    coba travis

--- www/application/config/auth-dev.php
+++ www/application/config/auth-dev.php
@@ -9,10 +9,11 @@ $config['google-redirecturi'] = $config['domain'] . '/auth/oauth2callback';
 
 $config['email-config'] = Array(
     'protocol' => 'smtp',
-    'smtp_host' => 'ssl://smtp.googlemail.com',
-    'smtp_port' => 465,
-    'smtp_user' => 'xxx',
-    'smtp_pass' => 'xxx',
+    'smtp_host' => 'smtp.googlemail.com',
+    'smtp_port' => 587,
+    'smtp_user' => 'rootbluetape@gmail.com', //masukan email google 
+    'smtp_pass' => 'Rootbluetape123', //password email  // di config bikin 2 AUTH 1 YANG SALAH
     'mailtype' => 'html',
-    'charset' => 'iso-8859-1'
+    'charset' => 'iso-8859-1',
+    'smtp_crypto'=> 'tls'
 );
--- www/application/controllers/UnitTest.php
+++ www/application/controllers/UnitTest.php
@@ -38,37 +38,37 @@
         */
        public function index(){
     
-            $this->cekJadwalByUsername('Dipo');
-            $this->cekYearMonthToSemesterCode();
-            $this->cekSemesterCodeToString();
-            $this->cekYearMonthToSemesterCodeSimplified();
-            $this->getName('anugrahjaya23@gmail.com');
-            $this->dbDateTimeToReadableDate();
-            $this->getEmail();
-            $this->cekGetNpm();
-            $this->cekGetAllJadwal();
-            $this->cekJadwalByJamMulai(7,0,'anugrahjaya23@gmail.com');
-			$this->checkRequestTypesForbidden();
-            $this->cekAddjadwal();
-			$this->checkKolomKeHari();
-            $this->checkHariKeKolom();
-            $this->cekGetNamaHari();
-            $this->cekGetNamaBulan();
-            $this->cekUpdateJadwal();
-            $this->cekGetNamaHari();
-            $this->cekGetNamaBulan();
-            $this->deleteByUsername('anugrahjaya23@gmail.com');
-            $this->cekDeleteJadwal(1);
-            $this->cekRequestByIdTranskrip(1,null,null);
-            $this->cekRequestByIdTranskrip(1,3,0);
-            $this->cekRequestBy('7316053@student.unpar.ac.id',null,null);
-            $this->cekRequestBy('7316053@student.unpar.ac.id',1,3);
-            $this->cekRequestByPerubahanKuliah('rootbluetape@gmail.com',null,null);
-            $this->cekRequestByPerubahanKuliah('rootbluetape@gmail.com',1,0);
+            // $this->cekJadwalByUsername('Dipo');
+            // $this->cekYearMonthToSemesterCode();
+            // $this->cekSemesterCodeToString();
+            // $this->cekYearMonthToSemesterCodeSimplified();
+            // $this->getName('anugrahjaya23@gmail.com');
+            // $this->dbDateTimeToReadableDate();
+            // $this->getEmail();
+            // $this->cekGetNpm();
+            // $this->cekGetAllJadwal();
+            // $this->cekJadwalByJamMulai(7,0,'anugrahjaya23@gmail.com');
+			// $this->checkRequestTypesForbidden();
+            // $this->cekAddjadwal();
+			// $this->checkKolomKeHari();
+            // $this->checkHariKeKolom();
+            // $this->cekGetNamaHari();
+            // $this->cekGetNamaBulan();
+            // $this->cekUpdateJadwal();
+            // $this->cekGetNamaHari();
+            // $this->cekGetNamaBulan();
+            // $this->deleteByUsername('anugrahjaya23@gmail.com');
+            // $this->cekDeleteJadwal(1);
+            // $this->cekRequestByIdTranskrip(1,null,null);
+            // $this->cekRequestByIdTranskrip(1,3,0);
+            // $this->cekRequestBy('7316053@student.unpar.ac.id',null,null);
+            // $this->cekRequestBy('7316053@student.unpar.ac.id',1,3);
+            // $this->cekRequestByPerubahanKuliah('rootbluetape@gmail.com',null,null);
+            // $this->cekRequestByPerubahanKuliah('rootbluetape@gmail.com',1,0);
             $this->cekSend_email();
-            $this->testLogout();
-            $this->testCreateAuthUrl();
-            $this->cekGetUserInfo();
+            // $this->testLogout();
+            // $this->testCreateAuthUrl();
+            // $this->cekGetUserInfo();
             $this->report();
         }
 
@@ -584,9 +584,8 @@
 
         public function cekSend_email(){
 
-            
-     // copy('../config/auth-test.php','../config/auth.php');
-            //test case jika=Debug true
+            copy('../config/auth-test.php','../config/auth.php');
+
             $email='gemini2911f665@gmail.com';
             $subject='Mengetes pengiriman pesan';
             $message='Tugas proyek informatika';
@@ -600,7 +599,6 @@
             $result2=$this->Email_model->send_email($email,$subject,$message,$debug2);
             $expected2=NULL;
             $this->unit->run($result2,$expected2,__FUNCTION__,'Test ini berfungsi untuk memeriksa apakah email sudah terkirim atau belum');
-            print_r($result2);
             //test case jika debug=false dan mail tidak terkirim
             $result3=$this->Email_model->send_email(NULL,NULL,NULL,$debug2);
             $expected3="Maaf, gagal mengirim email notifikasi.";
